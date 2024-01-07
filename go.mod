d7c3548360152fffe3a40b5d74cc152bde9354cc
jczornik <jczornik@graylight-imaging.com>
2024-01-07 10:45:49 +0100

    Cleanup

--- go.mod
+++ go.mod
@@ -2,17 +2,19 @@ module github.com/jczornik/glacier_backup
 
 go 1.21.5
 
-require github.com/go-yaml/yaml v2.1.0+incompatible
+require (
+	github.com/aws/aws-sdk-go-v2 v1.24.0
+	github.com/aws/aws-sdk-go-v2/config v1.26.2
+	github.com/aws/aws-sdk-go-v2/service/glacier v1.19.5
+	github.com/go-yaml/yaml v2.1.0+incompatible
+)
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.24.0 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.26.2 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.16.13 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.10 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.9 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.9 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.2 // indirect
-	github.com/aws/aws-sdk-go-v2/service/glacier v1.19.5 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.9 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.18.5 // indirect
--- main.go
+++ main.go
@@ -4,20 +4,29 @@ import (
 	"log"
 	"os"
 
+	"github.com/jczornik/glacier_backup/aws"
 	"github.com/jczornik/glacier_backup/backup"
 	"github.com/jczornik/glacier_backup/config"
 	"github.com/jczornik/glacier_backup/tools"
 )
 
+const (
+	ecRequiredTool = iota
+	ecConfigPath
+	ecReadingConf
+	ecCreatingBackup
+	ecUploadingBackup
+)
+
 func main() {
 	if err := tools.CheckRequiredTools(); err != nil {
-		log.Printf("Required prog does not exist. Err: %s\n", err)
-		os.Exit(2)
+		log.Printf("Required tool does not exist. Err: %s\n", err)
+		os.Exit(ecRequiredTool)
 	}
 
 	if len(os.Args) < 2 {
 		log.Println("Please specify path to configuration")
-		os.Exit(1)
+		os.Exit(ecConfigPath)
 	}
 
 	configPath := os.Args[1]
@@ -25,15 +34,18 @@ func main() {
 	cfg, err := config.NewConfig(configPath)
 	if err != nil {
 		log.Printf("Cannot read configuration. Err: %s\n", err)
-		os.Exit(3)
+		os.Exit(ecReadingConf)
 	}
 
 	for _, cbackup := range cfg.Backups {
 		if err := backup.CreateEncryptedBackup(cbackup.Src, cbackup.Dst, "1234"); err != nil {
 			log.Println(err)
-			os.Exit(4)
+			os.Exit(ecCreatingBackup)
 		}
 	}
 
-	//aws.UploadData(cfg)
+	if err := aws.UploadData(cfg); err != nil {
+		log.Println("Error while uploading backup")
+		os.Exit(ecUploadingBackup)
+	}
 }
