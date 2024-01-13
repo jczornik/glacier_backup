b4973cb65cb0d7f640c600b1babe51f7812250d1
jczornik <jczornik@graylight-imaging.com>
2024-01-13 06:56:16 +0100

    Add current time to backup filename

--- backup/backup.go
+++ backup/backup.go
@@ -3,6 +3,7 @@ package backup
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/jczornik/glacier_backup/config"
 	"github.com/jczornik/glacier_backup/tools"
@@ -10,6 +11,8 @@ import (
 
 func CreateEncryptedBackup(src config.BackupSrc, dst config.BackupDst, pass string) error {
 	bckName := lastPathElement(src)
+	now := time.Now().Format("2006-01-02_15-04-05")
+	bckName = fmt.Sprintf("%s_%s", bckName, now)
 	snapshot := fmt.Sprintf("%s/%s.manifest", dst, bckName)
 	encArchName := fmt.Sprintf("%s/%s.tar.gz.gpg", dst, bckName)
 
